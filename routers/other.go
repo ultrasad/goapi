4eca29eb461f743db2ec9d0b452a5a244c9a26a9
ultrasad <kissing-bear@hotmail.com>
2019-01-31 12:52:34 +0700

    test change route other

--- routers/other.go
+++ routers/other.go
@@ -13,4 +13,8 @@ func OtherRouter(e *echo.Echo) {
 	e.GET("/secret", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, "ช้างน้อย ฮานะจัง")
 	})
+	
+	e.POST("/secret3", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, "ช้างน้อย ฮานะจัง 3")
+	})
 }
